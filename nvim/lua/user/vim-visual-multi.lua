5fb85e32d25e4e8bb587b65462aca85cc9e99644
daneorie <588451+daneorie@users.noreply.github.com>
2022-08-18 10:46:46 -0400

    Temporarily removing some setup due to unresolved issues.

--- nvim/lua/user/vim-visual-multi.lua
+++ nvim/lua/user/vim-visual-multi.lua
@@ -1,4 +1,4 @@
-local g = vim.g
-g.VM_maps = {}
-g.VM_maps["Find Under"]         = "<C-d>"
-g.VM_maps["Find Subword Under"] = "<C-d>"
+--local g = vim.g
+--g.VM_maps = {}
+--g.VM_maps["Find Under"]         = "<C-d>"
+--g.VM_maps["Find Subword Under"] = "<C-d>"

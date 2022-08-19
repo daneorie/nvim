f94728830d050dfd1f9d3fc35b092f45594cf21b
daneorie <588451+daneorie@users.noreply.github.com>
2022-08-19 12:44:12 -0400

    vim-visual-multi keymaps must be done the vim.cmd way, unfortunately.

--- nvim/lua/user/vim-visual-multi.lua
+++ nvim/lua/user/vim-visual-multi.lua
@@ -1,3 +1,8 @@
+vim.cmd [[
+	let g:VM_maps = {}
+	let g:VM_maps["Find Under"]         = "<C-d>"
+	let g:VM_maps["Find Subword Under"] = "<C-d>"
+]]
 --local g = vim.g
 --g.VM_maps = {}
 --g.VM_maps["Find Under"]         = "<C-d>"

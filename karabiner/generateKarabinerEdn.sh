12f564b5c0db56ad85769e43be314a0e4ca387d3
daneorie <588451+daneorie@users.noreply.github.com>
2022-11-04 15:03:10 -0400

    Instead of plain JSON, goku (with .edn files) allows for simple, compact, and powerful syntax for creating complex modifications for Karabiner Elements.

---
+++ karabiner/generateKarabinerEdn.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+karabiner=(~/.config/karabiner.edn)
+
+cd ~/dotfiles/karabiner/
+
+echo "{" > $karabiner
+cat setup.edn >> $karabiner
+echo >> $karabiner
+echo ":main [" >> $karabiner
+cat spaceCadetShifts.edn >> $karabiner
+echo >> $karabiner
+cat spaceFn.edn >> $karabiner
+echo >> $karabiner
+cat colemak.edn >> $karabiner
+echo >> $karabiner
+cat hyperMeh.edn >> $karabiner
+echo "] ;; main" >> $karabiner
+echo "} ;; EOF">> $karabiner
+
+goku

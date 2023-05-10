7dbe04b1deb3ec4736118f19f072b294d18c375e
daneorie <588451+daneorie@users.noreply.github.com>
2023-05-10 01:10:22 -0400

    Added keybinds for opening the top 4 marked files. Overwrote a keybinding for toggleterm

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -3,3 +3,10 @@ local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>ma", mark.add_file)
 vim.keymap.set("n", "<leader>me", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<M-n>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<M-e>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<M-i>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<M-o>", function() ui.nav_file(4) end)
+
+vim.keymap.set("n", "<C-\\>", ":ToggleTerm<CR>")

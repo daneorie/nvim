b0755e4d4631ed41b92300e62d5700d762eb3ccf
daneorie <588451+daneorie@users.noreply.github.com>
2023-05-12 02:44:53 -0400

    Migrated nvim-tree keymaps to use on_attach

---
+++ nvim/after/plugin/nvim-tree.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>l", "<cmd>lua require('lsp_lines').toggle()<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>nt", ":NvimTreeToggle<CR>", {noremap = true, silent = true})

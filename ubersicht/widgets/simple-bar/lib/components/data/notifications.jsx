2da4960fc6bcbedaf741c042fdec2b5212c93f74
daneorie <588451+daneorie@users.noreply.github.com>
2023-12-04 23:05:04 -0500

    Fixed the notifications via Python not being able to be parsed correctly due to "<null>"

--- ubersicht/widgets/simple-bar/lib/components/data/notifications.jsx
+++ ubersicht/widgets/simple-bar/lib/components/data/notifications.jsx
@@ -18,84 +18,101 @@ const { refreshFrequency } = notificationWidgetOptions;
 
 const DEFAULT_REFRESH_FREQUENCY = 1000;
 const REFRESH_FREQUENCY = Settings.getRefreshFrequency(
-	refreshFrequency,
-	DEFAULT_REFRESH_FREQUENCY
+  refreshFrequency,
+  DEFAULT_REFRESH_FREQUENCY
 );
 
 let database;
 async function getDatabase() {
-	return (await Uebersicht.run(
-		`lsof -p "$(ps aux | grep -m1 usernoted | awk '{ print $2 }')" | awk '{ print $NF }' | grep 'db2/db$'`
-	)).trim();
+  return (
+    await Uebersicht.run(
+      `lsof -p "$(ps aux | grep -m1 usernoted | awk '{ print $2 }')" | awk '{ print $NF }' | grep 'db2/db$'`
+    )
+  ).trim();
 }
 
 const openApp = (bundleIdentifier) =>
-	Uebersicht.run(
-		`open -b ${bundleIdentifier}`
-	);
+  Uebersicht.run(`open -b ${bundleIdentifier}`);
 
 export const Widget = () => {
-	const [state, setState] = Uebersicht.React.useState({});
-	const [loading, setLoading] = Uebersicht.React.useState(notificationWidget);
+  const [state, setState] = Uebersicht.React.useState({});
+  const [loading, setLoading] = Uebersicht.React.useState(notificationWidget);
 
-	const getNotifications = async () => {
-		// Sometimes, when trying to get the database, the command doesn't return anything,
-		//   so once we get it successfully once, we should keep the value.
-		if (!database) database = await getDatabase();
+  const getNotifications = async () => {
+    // Sometimes, when trying to get the database, the command doesn't return anything,
+    //   so once we get it successfully once, we should keep the value.
+    if (!database) database = await getDatabase();
 
-		// handle default execution
-		const defaultList =	Object.keys(AppNotifications.methods.default)
-			.filter(appName => notificationWidgetOptions[AppOptions.apps[appName]])
-			.map(appName => AppIdentifiers.apps[appName]);
-		const defaultResponse = await Uebersicht.run(
-			`./simple-bar/lib/scripts/notifications-default.sh "${database}" "${defaultList.join("', '")}"`
-		);
-		const defaultAppBadgeJsonList = JSON.parse(defaultResponse);
-		function getAppNameByIdentifier(object, value) {
-			return Object.keys(object).find(key => object[key] === value)
-		}
-		defaultAppBadgeJsonList.forEach(appObject => {
-			const appName = getAppNameByIdentifier(AppIdentifiers.apps, appObject.identifier);
-			setState(state => ({...state, [appName]: appObject.badge }));
-		});
+    // handle default execution
+    const defaultList = Object.keys(AppNotifications.methods.default)
+      .filter((appName) => notificationWidgetOptions[AppOptions.apps[appName]])
+      .map((appName) => AppIdentifiers.apps[appName]);
+    const defaultResponse = await Uebersicht.run(
+      `./simple-bar/lib/scripts/notifications-default.sh "${database}" "${defaultList.join(
+        "', '"
+      )}"`
+    );
+    const defaultAppBadgeJsonList = JSON.parse(defaultResponse);
+    function getAppNameByIdentifier(object, value) {
+      return Object.keys(object).find((key) => object[key] === value);
+    }
+    defaultAppBadgeJsonList.forEach((appObject) => {
+      const appName = getAppNameByIdentifier(
+        AppIdentifiers.apps,
+        appObject.identifier
+      );
+      setState((state) => ({ ...state, [appName]: appObject.badge }));
+    });
 
-		// handle python execution
-		const pythonResponse = await Uebersicht.run(
-			`./simple-bar/lib/scripts/notifications-other.py3`
-		);
-		const pythonAppBadgeJsonList = JSON.parse(pythonResponse.replace(/'/g, '"'));
-		Object.keys(AppNotifications.methods.python)
-			.filter(appName => notificationWidgetOptions[AppOptions.apps[appName]])
-			.forEach(appName => 
-				setState(state => ({...state, [appName]: pythonAppBadgeJsonList[appName] }))
-			);
+    // handle python execution
+    const pythonResponse = await Uebersicht.run(
+      `./simple-bar/lib/scripts/notifications-other.py3`
+    );
+    const pythonAppBadgeJsonList = JSON.parse(
+      pythonResponse.replace(/'/g, '"').replace(/<null>/g, "0")
+    );
+    Object.keys(AppNotifications.methods.python)
+      .filter((appName) => notificationWidgetOptions[AppOptions.apps[appName]])
+      .forEach((appName) =>
+        setState((state) => ({
+          ...state,
+          [appName]: pythonAppBadgeJsonList[appName],
+        }))
+      );
 
-		setLoading(false);
-	};
+    setLoading(false);
+  };
 
-	useWidgetRefresh(notificationWidget, getNotifications, REFRESH_FREQUENCY);
+  useWidgetRefresh(notificationWidget, getNotifications, REFRESH_FREQUENCY);
 
-	if (loading) return <DataWidgetLoader.Widget className="notification" />;
-	if (!state) return null;
+  if (loading) return <DataWidgetLoader.Widget className="notification" />;
+  if (!state) return null;
 
-	const onClick = (bundleIdentifier) => (e) => {
-		Utils.clickEffect(e);
-		openApp(bundleIdentifier);
-	};
+  const onClick = (bundleIdentifier) => (e) => {
+    Utils.clickEffect(e);
+    openApp(bundleIdentifier);
+  };
 
-	return (
-		<DataWidget.Widget classes="notifications">
-			{Object.keys(state)
-				.filter(appName => state[appName] && notificationWidgetOptions[AppOptions.apps[appName]])
-				.map((appName, _) => {
-					const Icon = Icons[appName.replace(/ /, "")] || Icons.Default;
-					return <div onClick={onClick(AppIdentifiers.apps[appName])} className="notification">
-						<Icon />
-						{state[appName]}
-					</div>
-				})
-			}
-		</DataWidget.Widget>
-	)
-
-}
+  return (
+    <DataWidget.Widget classes="notifications">
+      {Object.keys(state)
+        .filter(
+          (appName) =>
+            state[appName] &&
+            notificationWidgetOptions[AppOptions.apps[appName]]
+        )
+        .map((appName, _) => {
+          const Icon = Icons[appName.replace(/ /, "")] || Icons.Default;
+          return (
+            <div
+              onClick={onClick(AppIdentifiers.apps[appName])}
+              className="notification"
+            >
+              <Icon />
+              {state[appName]}
+            </div>
+          );
+        })}
+    </DataWidget.Widget>
+  );
+};
